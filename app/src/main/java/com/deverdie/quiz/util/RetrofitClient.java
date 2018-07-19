ddf67cb105161b45bc7d34849f3daebe0be38f76
tanakphong <t.phongsing@gmail.com>
2018-07-19 13:28:07 +0700

    Initial commit and library retrofit

---
+++ app/src/main/java/com/deverdie/quiz/util/RetrofitClient.java
@@ -0,0 +1,24 @@
+package com.deverdie.quiz.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String URL = "https://demo0937961.mockable.io/";
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        return new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+    }
+}
